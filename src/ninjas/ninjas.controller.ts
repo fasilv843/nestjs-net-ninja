85c51ba14993be01e1d29fd9e5a655ff3da735f0
Fasil <fasilvaliyattil843@gmail.com>
2024-03-20 15:52:06 +0530

    apis with controller, service and exception

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
-@Controller('ninjas')
-export class NinjasController {}
+@Controller('ninjas') // everything in this controller have this prefix // or route start with ninjas in this controller
+export class NinjasController {
+  constructor(private readonly _ninjaService: NinjasService) {}
+
+  @Get()
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    return this._ninjaService.getNinjas(weapon);
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    try {
+      return this._ninjaService.getNinja(+id);
+    } catch (error) {
+      throw new NotFoundException();
+    }
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return this._ninjaService.createNinja(createNinjaDto);
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this._ninjaService.updateNinja(+id, updateNinjaDto);
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return this._ninjaService.removeNinja(+id);
+  }
+}
